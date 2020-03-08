de32a59c14938bdeefa4a2ecf53a4b0d66e8bf95
zhangmeng <569866591@qq.com>
2020-03-08 22:50:36 +0800

    LeetCode每日一题

---
+++ src/main/java/com/zzm/leetcode/array/Question322.java
@@ -0,0 +1,63 @@
+package com.zzm.leetcode.array;
+
+/**
+ * @author: zhangmeng
+ * @date: 2020/3/8 21:58
+ * @Description:
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+
+示例 1:
+
+输入: coins = [1, 2, 5], amount = 11
+输出: 3
+解释: 11 = 5 + 5 + 1
+示例 2:
+
+输入: coins = [2], amount = 3
+输出: -1
+
+ *
+ *
+ *
+ *
+ */
+public class Question322 {
+
+
+    public static void main(String[] args) {
+        int[] coins = new int[]{1,2,5};
+        int amount = 11;
+        int result = coinChange(coins,amount);
+        System.out.println(result);
+
+
+    }
+
+
+    public static int coinChange(int[] coins, int amount) {
+
+        // 自底向上的动态规划
+        if(coins.length == 0){
+            return -1;
+        }
+
+        // memo[n]的值： 表示的凑成总金额为n所需的最少的硬币个数
+        int[] memo = new int[amount+1];
+        memo[0] = 0;
+        for(int i = 1; i <= amount;i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0;j < coins.length;j++){
+                if(i - coins[j] >= 0 && memo[i-coins[j]] < min){
+                    min = memo[i-coins[j]] + 1;
+                }
+            }
+            // memo[i] = (min == Integer.MAX_VALUE ? Integer.MAX_VALUE : min);
+            memo[i] = min;
+        }
+
+        return memo[amount] == Integer.MAX_VALUE ? -1 : memo[amount];
+
+    }
+
+
+}
